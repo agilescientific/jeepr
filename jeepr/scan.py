70fc304b81abf1969ad65e2c02e02349c2b90249
kwinkunks <matt@agilegeoscience.com>
2017-11-03 15:43:56 -0300

    basic plotting

--- jeepr/scan.py
+++ jeepr/scan.py
@@ -8,6 +8,7 @@ Defines scan objects.
 import random
 
 import numpy as np
+import matplotlib.pyplot as plt
 import h5py
 
 from . import utils
@@ -48,7 +49,11 @@ class Scan(np.ndarray):
         if obj.size == 1:
             return float(obj)
 
+        # The atrtibutes you want child objects to inherit, eg after slicing.
+        # This could go wrong... may need some sanity checking.
         self.dt = getattr(obj, 'dt', 0)
+        self.dx = getattr(obj, 'dx', 0)
+        self.domain = getattr(obj, 'domain', 0)
         self.name = getattr(obj, 'name', '')
 
     def __copy__(self):
@@ -71,7 +76,6 @@ class Scan(np.ndarray):
         meta, _, arr = read_rad(fname)
         dt = meta['SPR_SAMPLING_INTERVAL']  # in picoseconds
         dx = meta['SPR_SHAFT_INTERVAL']     # in metres...
-        units = meta['UNITS']               # ...maybe
 
         params = {'dt': float(dt) * 1e-12,
                   'dx': float(dx),
@@ -79,7 +83,7 @@ class Scan(np.ndarray):
                   'meta': meta,
                   }
 
-        return cls(arr.T, params)
+        return cls(arr.astype(np.float).T, params)
 
     @classmethod
     def from_gprmax(cls, fname):
@@ -87,7 +91,7 @@ class Scan(np.ndarray):
         Constructor for merged gprMax .out files.
 
         Args:
-            fname (str): a RAD file.
+            fname (str): a gprMax OUT file.
 
         Returns:
             scan. The scan object.
@@ -108,14 +112,20 @@ class Scan(np.ndarray):
                   'meta': {'nrx': nrx},
                   }
 
-        return cls(arr, params)
+        return cls(arr.astype(np.float), params)
 
     @property
     def time(self):
+        """
+        The time basis of the scan. Computed from dt and shape.
+        """
         return utils.srange(0, self.dt, self.shape[0])
 
     @property
     def nx(self):
+        """
+        The number of x samples.
+        """
         try:
             return self.shape[1]
         except IndexError:
@@ -123,11 +133,58 @@ class Scan(np.ndarray):
 
     @property
     def x(self):
+        """
+        The spatial basis of the scan.
+        """
         return utils.srange(0, self.dx, self.nx)
 
     @property
     def extent(self):
+        """
+        The extent. Useful for ``plt.imshow(scan, extent=extent)``.
+        """
         return [self.x[0], self.x[-1], self.time[-1]*1e9, self.time[0]*1e9]
+
+    def plot(self, ax=None, return_fig=False, percentile=99):
+        """
+        Plot a scan.
+
+        Args:
+            ax (ax): A matplotlib axis.
+            return_fig (bool): whether to return the matplotlib figure.
+                Default False.
+
+        Returns:
+            ax. If you passed in an ax, otherwise None.
+        """
+        if ax is None:
+            fig = plt.figure(figsize=(16, 9))
+            ax = fig.add_subplot(111)
+            return_ax = False
+        else:
+            return_ax = True
+
+        ma = np.percentile(self, percentile)
+        im = ax.imshow(self,
+                       origin='upper',
+                       extent=self.extent,
+                       aspect='auto',
+                       vmin=-ma, vmax=ma
+                       )
+        _ = plt.colorbar(im)
+        ax.set_xlabel('x position (m)', size=16)
+        if self.domain == 'time':
+            ax.set_ylabel('two-way time (ns)', size=16)
+        else:
+            ax.set_ylabel('estimated depth (m)', size=16)
+        ax.set_title(self.name, size=16)
+
+        if return_ax:
+            return ax
+        elif return_fig:
+            return fig
+        else:
+            return None
 
     def add_model(self, arr, params):
         """
@@ -143,12 +200,46 @@ class Scan(np.ndarray):
         """
         pass
 
+    def demean(self):
+        """
+        Subtracts the mean traces from the scan.
+
+        Returns:
+            Scan.
+        """
+        return self - np.mean(self, axis=1, keepdims=True)
+
+    def gain(self, method=None):
+        """
+        Applies one of a few gain functions.
+
+        Args:
+            method (str): Which method to use. "linear", "reciprocal",
+                or "power".
+
+        Returns:
+            Scan.
+        """
+        nt = self.shape[0]
+        if (method is None) or (method == "linear"):
+            f = (np.arange(nt)/nt).reshape(nt, 1)
+        if method == "reciprocal":
+            f = ((1 - 1/np.arange(nt))**12).reshape(nt, 1)
+        if method == "power":
+            f = ((np.arange(nt)/512)**2).reshape(nt, 1)
+        return self.astype(np.float64) * f
+
     def get_spectrum(self, n=None, return_amp=False):
         """
         Get a power spectrum for the scan.
 
         Args:
             n (int): Number of traces to use, default 10% or at least 10.
+            return_amp (bool): Whether to return amplitude instead of power.
+
+        Returns:
+            tuple: (ndarray, ndarray), the frequencies and power (or amplitude)
+                values. Often, you want to do plt.plot(f, p) next.
         """
         if n is None:
             n = 0.1
